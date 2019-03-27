fa19c884997707cbfa24d9e447e0d9b3c8412335
researchofhemanth <48641743+researchofhemanth@users.noreply.github.com>
2019-03-27 12:15:02 +0530

    phish.py
    4th,5th,6th Column added

--- phish.py
+++ phish.py
@@ -3,6 +3,28 @@ def URLhavingattherate(URL): #4th column
         return -1
     else:
         return 1
+def Redirectusingdoubleslash(URL):#5th column
+    for i in range(len(URL)-1):
+        if URL[i]=="/" and URL[i+1]=="/":
+            p=i
+    if p>7:
+        return -1
+    else:
+        return 1
+def prefixsuffix(URL): #6th column
+    if '-' in URL:
+         return -1
+    else:
+        return 1
 url = input()
+L=[]
+for i in range(30):
+    L+=[-1]
 p = URLhavingattherate(url)
-print(p)
+L[3]=p
+k = Redirectusingdoubleslash(url)
+L[4]=k
+s = prefixsuffix(url)
+L[5]=s
+
+print(*L)
